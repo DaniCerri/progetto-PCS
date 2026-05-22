5cde921915622a0eafc52fb8c60ec776735a9ba0
Daniele Cerrina <daniecerri@gmail.com>
2026-05-22 10:05:40 +0200

    Primo setup parser

---
+++ source/parser/parser.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+class Parser {
+    // std::string output;  // TODO 
+
+    private: 
+    
+
+    public:
+        Parser() = default;
+        ~Parser() = default;
+
+        void parse();
+}
+    
